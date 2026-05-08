aa7d13c6b9347cd5ef26c3f68062151232770c68
rokan45 <rokanuzzaman4567@gmail.com>
2026-05-08 18:44:15 +0600

    enviroment set for the userdata handle and create signup page component

---
+++ src/app/signup/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignupPage = () => {
+    return (
+        <div>
+            <h2>Hello signup page</h2>
+        </div>
+    );
+};
+
+export default SignupPage;
